d7723ec33fe76a20c69bdd7dd0409eb3f1a5ec23
guoxiaona <398182130@qq.com>
2025-11-05 05:23:08 +0000

    feat: fix compiled error for lusitana font issue

--- nextjs-dashboard/app/ui/fonts.ts
+++ nextjs-dashboard/app/ui/fonts.ts
@@ -1,4 +1,8 @@
 import { Inter, Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({ weight: '700' });
+export const lusitana = Lusitana({ 
+  weight: ['400', '700'], // 同时加载普通和粗体
+  subsets: ['latin'],
+  display: 'swap'
+});
